207a25899a52a5d5cece36a304cdf7c5bf77a3b1
unknown <jeff920422@gmail.com>
2023-12-30 17:25:38 +0800

    add the host_btn sound effect

--- app/src/main/java/com/example/project_ble/MainActivity.java
+++ app/src/main/java/com/example/project_ble/MainActivity.java
@@ -15,6 +15,7 @@ import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -22,12 +23,12 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityResultLauncher<Intent> enableBluetoothLauncher;
-
+    private MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         enableBluetoothLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 result -> {
@@ -46,6 +47,19 @@ public class MainActivity extends AppCompatActivity {
         Button btn_user = findViewById(R.id.btn_user);
 
         btn_host.setOnClickListener(v -> {
+            mediaPlayer = MediaPlayer.create(this, R.raw.click_sound);
+            playClickSound();
+            Intent intent = new Intent(this, HostActivity.class);
+            startActivity(intent);
+        });
+
+        btn_user.setOnClickListener(v -> {
+            playClickSound();
+            Intent intent = new Intent(this, UserActivity.class);
+            startActivity(intent);
+        });
+
+        btn_host.setOnClickListener(v -> {
             Intent intent = new Intent(this, HostActivity.class);
             startActivity(intent);
         });
@@ -54,6 +68,19 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, UserActivity.class);
             startActivity(intent);
         });
+    }
+    private void playClickSound() {
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        super.onDestroy();
     }
 
     private void checkAndAskPermission(){
